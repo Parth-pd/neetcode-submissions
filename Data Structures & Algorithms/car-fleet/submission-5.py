79ef43e47015fe93e1c5b254bead3ccb602f3b67
Parth Dobariya <parth.dobariya.5127@gmail.com>
2026-06-15 12:00:57 +0530

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #brute force LOL
+        fleets = defaultdict(list)
+        n = len(position)
+        
+        h ={}
+        for i in range(n):
+            h[position[i]] = speed[i] 
+
+        correct = dict(sorted(h.items()))
+        sortedCars = list(correct.items())
+
+        stack = []
+        for i in range(n - 1, -1, -1):
+
+            currpos, currspeed = sortedCars[i]
+            
+            currT = (target - currpos) / currspeed
+            if stack:
+                lastT = stack.pop()
+                if lastT > currT:
+                    fleets[lastT].append(i)
+                    stack.append(lastT)
+                else:
+                    fleets[currT].append(i)
+                    stack.append(currT)
+            else:
+                fleets[currT].append(i)
+                stack.append(currT)
+        return (len(fleets))
