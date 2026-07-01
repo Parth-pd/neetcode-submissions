ee8fe9fd134ba71a07f5905429b15dfe3f9a9d2c
Parth Dobariya <parth.dobariya.5127@gmail.com>
2026-07-01 23:06:21 +0530

    Add: frequency-of-the-most-frequent-element - submission-11

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        nums.sort()
+        maxL = 1
+        left, right, total = 0,0,0
+        while right < n:
+            total += nums[right]
+            while nums[right] * (right - left + 1) - total > k:
+                total -= nums[left]
+                left += 1
+            maxL = max(maxL, right - left + 1) 
+            right += 1
+        return maxL
